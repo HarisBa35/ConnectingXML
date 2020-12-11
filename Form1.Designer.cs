db89851a87ee9ab91fc1e2eaeb6fd06251a0246f
Haris Basic <haris.basic67@gmail.com>
2020-12-11 18:36:23 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spoji exporte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Prebaci u folder za spajanje";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(102, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(607, 91);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 195);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+
+
+        }
+        //Spajanje exporta
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles(@"C:\Terminal_razmjena\Prenos_na_terminal");
+            if (filePaths.Length == 0)
+            {
+                MessageBox.Show("Niste exportovali drugi export iz IMIS-a!");
+                return;
+            }
+            XElement xFileRoot = XElement.Load(@"C:\Users\fakturno1\Desktop\New folder (4)\export.xml");
+            XElement xFileChild = XElement.Load(@"C:\Terminal_razmjena\Prenos_na_terminal\export.xml");
+            var header = xFileChild.Elements("zaglavlje");
+            xFileRoot.Add(header);
+            xFileRoot.Save(@"C:\Terminal_razmjena\Prenos_na_terminal\export.xml");
+            label1.Text = "Exporti su spojeni. Izvršite slanje exporta na prenosni terminal!";
+            File.Delete(@"C:\Users\fakturno1\Desktop\New folder (4)\export.xml");
+            button1.Enabled = false;
+
+        }
+        
+        // Prebacivanje u folder za spajanje       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileToCopy = @"C:\Terminal_razmjena\Prenos_na_terminal\export.xml";
+            string destinationDirectory = @"C:\Users\fakturno1\Desktop\New folder (4)\export.xml";
+            string[] filePathsPrebaci = Directory.GetFiles(@"C:\Terminal_razmjena\Prenos_na_terminal");
+            if (filePathsPrebaci.Length == 0)
+            {
+                MessageBox.Show("Nema exporta u folderu!");
+                return;
+            }
+            File.Move(fileToCopy, destinationDirectory);
+            MessageBox.Show("Export je prebačen u folder za spajanje, sad exportujte drugi export iz IMIS-a");
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string MessageBoxTitle = "Spajanje exporta!!!";
+            string MessageBoxContent = "Jeste li sigurni da ste završili spajanje exporta???";
+
+            DialogResult dialogResult = MessageBox.Show(MessageBoxContent, MessageBoxTitle, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else if (dialogResult == DialogResult.Yes)
+            {
+               
+                Application.ExitThread();
+            }
+
+        }
+    }
+    
+}
